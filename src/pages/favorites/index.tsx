d88bed52a64b2af67f12756790f68dc3d434f0c8
Diego Sagredo <dsagredog@gmail.com>
2023-04-07 22:54:30 -0400

    feat: update v1

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from '@/components/layouts';
 import NoFavorites from '@/components/ui/NoFavorites';
 import { localFavorites } from '@/utils';
