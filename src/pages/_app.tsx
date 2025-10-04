8a2e15991b489f3b589666379cf3f1f3c1315d3f
Diego Sagredo <dsagredog@gmail.com>
2025-10-04 14:06:55 -0300

    Updated tailwind.config.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,12 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], weight: ['400', '600', '700', '800', '900'] });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <main className={inter.className}>
-            <NextThemesProvider attribute="class" defaultTheme="system">
+            <NextThemesProvider attribute="class" defaultTheme="dark">
                 <HeroUIProvider>
                     <Component {...pageProps} />
                 </HeroUIProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@ import { heroui } from '@heroui/react';
 
 export default {
     content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-        './app/**/*.{js,ts,jsx,tsx}',
+        './src/pages/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
+        './src/app/**/*.{js,ts,jsx,tsx}',
+        './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     darkMode: 'class',
     theme: {
